5767ffc4045a5224084f918927998c2d86d4d6d5
George Park <georgewpark@outlook.com>
2022-10-01 16:07:00 +0100

    remove unused plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin")
 const TerserPlugin = require("terser-webpack-plugin")
-const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
   entry: {
